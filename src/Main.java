5ac161cddfc0f6821b102c44a0a90b8d8dcc6bff
Marcos Capistrano <mcapistrano50@gmail.com>
2026-06-16 18:27:02 -0300

    comecandoo

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,5 @@ import javax.swing.*;
 import java.awt.*;
 
 public static void main(String[] args) {
-    JFrame janela = new JFrame("Ecossistema");
-    janela.setSize(800, 680);
-    janela.setLocationRelativeTo(null);
-    janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    janela.setLayout(new BorderLayout());
 
-    JPanel painel = new JPanel();
-    painel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
-
-    JButton botao = new JButton("Avançar");
-    painel.add(botao);
-
-    janela.add(painel, BorderLayout.NORTH);
-    janela.add(new Graficos(), BorderLayout.CENTER);
-
-    janela.setVisible(true);
 }
